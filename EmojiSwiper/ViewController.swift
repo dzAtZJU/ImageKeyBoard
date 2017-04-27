140c2b29941e7c586339303870a36ade2349640c
dzAtZJU <travechow@gmail.com>
2017-04-27 08:34:32 +0800

    Initial Commit

---
+++ EmojiSwiper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiSwiper
+//
+//  Created by zz on 27/04/2017.
+//  Copyright © 2017 Zhou Wei Ran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
